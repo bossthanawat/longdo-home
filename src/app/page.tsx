09fbb69c6a3103a0015fbe918bc0d300e19c4fe8
bossthanawat <boss41680@gmail.com>
2024-03-20 13:03:29 +0700

    Add @vercel/postgres dependency and update package.json and pnpm-lock.yaml

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
 import Map from "@/components/maptiler-map/Map";
 import Image from "next/image";
+import raw from "../../raw/converted.json";
 
-export default function Home() {
+async function getData() {
+  const result = raw as any;
+  return result[0];
+}
+
+export default async function Home() {
+  const data = await getData();
+  console.log(data);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Map />
+      {/* <span>{data}</span> */}
+      {/* <Map /> */}
     </main>
   );
 }
